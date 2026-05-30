cfac2b5a020d5fee44805baf9dc5563617f5be02
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-05-30 18:03:16 +0100

    création du projet et présentation de python

---
+++ 01_presentation.py
@@ -0,0 +1,5 @@
+# Mon premier commentaire en Python
+
+print("Hello World")
+print("Je m'appelle", "Abdoul Rachid")
+print("Je réapprends Python en", 2026)
---
+++ test.py
@@ -0,0 +1,2 @@
+print("Bonjour ! Je suis de retour en Python 😍")
+print("Version Python :", __import__('sys').version.split()[0])
