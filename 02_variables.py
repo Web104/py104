dffc92320ba1fc43a4774b8911467fb7cf4df3b8
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-05-30 18:28:45 +0100

    les variables en python

---
+++ 02_variables.py
@@ -0,0 +1,18 @@
+# Variables en Python
+
+nom = "Abdoul Rachid"   # Chaîne de caractère (str)
+age = 25                # Nom entier (int)
+taille = 1.75           # Nombre décimal (float)
+est_etudiant = True     # Booléen (bool)
+
+print("Nom : ", nom)
+print("Âge :", age)
+print("Taille : ", taille, "m")
+print("Est étudiant", est_etudiant)
+
+# Vérifier le type d'une variable
+print("\nTypes des variables :")
+print("nom ->", type(nom))
+print("age ->", type(age))
+print("taille ->", type(taille))
+print("est_étudiant ->", type(est_etudiant))
