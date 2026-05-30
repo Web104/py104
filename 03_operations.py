e5b4fa9497cdcd52d1bd1138872e0eaa7e46f86e
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-05-30 19:13:15 +0100

    études des opérations en python

---
+++ 03_operations.py
@@ -0,0 +1,23 @@
+# Opérations mathématiques
+
+a = 15
+b = 4
+print("Addition :", a + b)
+print("Soustraction :", a - b)
+print("Multiplication", a * b)
+print("Division", a / b)        # Division classiques
+print("Division entière :", a // b)
+print("Modulo :", a % b)        # Rested de la division
+print("Puissance :", a ** 2)
+
+# Chaînes de caractères
+prenom = "Abdoul" 
+nom = "Rachid"
+
+# Ancienne façon (moins recommandée aujourd'hui)
+print("Je m'appelle " + prenom + " " + nom)
+
+# Nouvelle façon moderne : f-strings (très importante)  en python 3.6+
+print(f"Je m'appelle {prenom} {nom}")
+print(f"J'ai {2026-2002} ans en 2026")
+
